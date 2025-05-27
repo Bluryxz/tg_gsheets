e9bfd1547ebe417c747d5937a4858464282e8cdc
Bluryxz <rubakruta@gmail.com>
2025-05-27 10:15:28 +0500

    working (no formatting)

--- app/gsheets.py
+++ app/gsheets.py
@@ -24,13 +24,22 @@ async def sheet_write(agcm, name, date, client, employee, responsible):
     sh = await agc.open("апи тест")
     sh = await sh.get_worksheet(0) 
     
+    number = 1
     i = 1
     cell = await sh.cell(i, 1)
 
     while cell.value != None:
+
+        try:
+            if int(cell.value) > number:
+                number = int(cell.value) + 1
+        except ValueError:
+            pass
+
         i += 1
         cell = await sh.cell(i, 1)
 
+    await sh.update_cell(i, 1, number)
     await sh.update_cell(i, 2, name)
     await sh.update_cell(i, 7, date)
     await sh.update_cell(i, 9, client)
--- app/handlers.py
+++ app/handlers.py
@@ -11,17 +11,19 @@ rt = Router()
 async def write(message: Message):
     text = message.text
 
-    date = message.date
+    date = str(message.date)[:10]
+    date = date[8:10] + '.' + date[5:7] + '.' + date[:4]
+
     client_pos = message.text.lower().find('контрагент')
     employee_pos = message.text.lower().find('сотрудник')
     responsible_pos = message.text.lower().find('исполнитель')
 
-    name = text[:client_pos - 1]
-    client = text[client_pos : employee_pos - 1]
-    employee = text[employee_pos : responsible_pos - 1]
-    responsible = text[responsible_pos:]
+    name = text[:client_pos]
+    client = text[client_pos + 11:employee_pos - 1]
+    employee = text[employee_pos + 10:responsible_pos - 1]
+    responsible = text[responsible_pos + 12:]
 
-    print(name, date, client, employee, responsible)
+    print(name + '\n', date, client + '\n', employee + '\n', responsible)
     await sheet_write(agcm, name, date, client, employee, responsible)
 
     await message.react(
